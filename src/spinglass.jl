d72366e5a459762407c95723940fccccb14553c8
Jaden Jang <jadenjang26@gmail.com>
2026-09-10 23:29:05 -0400

    constructor can now construct sparse graphs

--- src/spinglass.jl
+++ src/spinglass.jl
@@ -25,6 +25,26 @@ function curie_weiss(n::Int64)
   return J
 end
 
+# spare graph function
+function render_sparse(J::Matrix{Float64}, p::Float64)
+  println("We will kill all connections with probability $p")
+  # exception handling
+  if p < 0 || p >= 1.0
+    throw(DomainError(p, "probabilities must be [0,1)"))
+  end
+
+  # kill with probability p all J_ij
+  # J .*= (rand(size(J)) .> p)
+  sz = size(J, 1)
+  for i in 1:sz
+    for j in i:sz
+      J[i, j] *= floor(rand()+(1-p))
+      J[j, i] = J[i, j]
+    end
+  end
+  return J
+end
+
 # helper functions: generate random field h_i
 function random_field(n::Int64, h0::Float64=1.0)
   return randn(n) .* h0
